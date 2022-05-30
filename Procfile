efc3eea0ef164c517785f055940fbefa123e654e
searinox0902 <svquintana98@gmail.com>
2022-05-30 13:34:02 -0500

    Rest env

--- Procfile
+++
@@ -1,1 +0,0 @@
-web gunicorn tienda_online.wsgi
--- contacto/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class Contacto(forms.Form):
-
-    asunto = forms.CharField()
-    correo = forms.EmailField()
-    mensaje = forms.CharField()
